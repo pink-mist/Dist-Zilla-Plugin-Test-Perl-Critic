46d5848a5cdd5806d9db92f5017bc356ddbb7eae
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2017-06-11 22:17:04 +0200

    specify severity in test as well

--- lib/Dist/Zilla/Plugin/Test/Perl/Critic/Freenode.pm
+++ lib/Dist/Zilla/Plugin/Test/Perl/Critic/Freenode.pm
@@ -123,5 +123,5 @@ ___[ xt/author/critic-freenode.t ]___
 use strict;
 use warnings;
 
-use Test::Perl::Critic (-theme => 'freenode');
+use Test::Perl::Critic (-theme => 'freenode', severity => 1);
 all_critic_ok();
